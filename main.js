8aa5fbbe8b176c0e56aa8e4b6268a66a3e7af659
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-01-11 17:49:58 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -37,11 +37,68 @@ Webcam.set({
     
     synth = window.speechSynthesis;
     
-    speakData1 = "the first prediction is " + prediction1;
-    speakData2 = "and the second prediction is " + prediction2;
+    speakData1 = "the prediction is " + prediction1;
     
-    utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
+    utterThis = new SpeechSynthesisUtterance(speakData1);
     
     synth.speak(utterThis);
     
     }
+
+        
+        function check(){
+           
+            img = document.getElementById("imgresult");
+        
+            classifier.classify(img, gotResult);
+        }
+        
+        function gotResult(error, results){
+        
+        if(error){
+        
+        console.error(error);
+        
+        }
+        else{
+        
+        console.log(results);
+        
+        document.getElementById("result_name").innerHTML = results[0].label;
+        
+        prediction1 = results[0].label;
+    
+        
+        speak();
+        
+        if(results[0].label == "Thumbs up"){
+        
+        document.getElementById("result_img").innerHTML = "&#128077;";
+        
+        }
+        else if(results[0].label == "Ok"){
+        
+            document.getElementById("result_img").innerHTML = "&#128076;";
+        
+        }
+          else if(results[0].label == "Stop"){
+        
+            document.getElementById("result_img").innerHTML = "&#9995;";
+        
+        }
+          else if(results[0].label == "Pointing"){
+        
+            document.getElementById("result_img").innerHTML = "&#9755;";
+        
+        }
+        else{
+        
+            document.getElementById("result_img").innerHTML = "&#9994;";
+        
+        }
+        
+        
+        
+        }
+        
+        }
